274a600cd20bead4781e1045a07cb966a8ac839d
symys <nphenry.dev@proton.me>
2024-05-30 02:09:24 -0500

    Added basic bruteforcing addon

---
+++ README.md
@@ -0,0 +1,9 @@
+This is my collection of addons I've created for mitmproxy!!
+Look, these aren't the greatest code quality in the world.
+They're really designed to make my own life easier, but I'll still share them with the world.
+I kinda hate burpsuite but I like mitmproxy.
+So, sometimes I hack together addons to expand its capabilities and make it smoother to use other programs
+like ffuf by populating it with values from mitmproxy...
+But they're quick and dirty. If I find a bug, I might fix it, or I might just live with it if it's not any work to avoid it ¯\_(ツ)_/¯
+
+That being said, help yourself!
---
+++ bruteforce.py
@@ -0,0 +1,55 @@
+"""Brute-force two request fields from two wordlists"""
+
+import logging
+import subprocess
+from collections.abc import Sequence
+from typing import BinaryIO
+
+from mitmproxy import io
+from mitmproxy import command
+from mitmproxy import flow
+from mitmproxy import http
+from mitmproxy import types
+from mitmproxy.log import ALERT
+from mitmproxy.net.http.http1 import assemble
+from mitmproxy import exceptions
+
+words_A = []
+words_B = []
+logger = logging.getLogger(__name__)
+#these two functions just copypasta'd from mitmproxy/addons/export.py
+def cleanup_request(f: flow.Flow) -> http.Request:
+    if not getattr(f, "request", None):
+        raise exceptions.CommandError("Can't export flow with no request.")
+    assert isinstance(f, http.HTTPFlow)
+    request = f.request.copy()
+    request.decode(strict=False)
+    return request
+
+def raw_request(f: flow.Flow) -> bytes:
+    request = cleanup_request(f)
+    if request.raw_content is None:
+        raise exceptions.CommandError("Request content missing.")
+    return assemble.assemble_request(request)
+
+class BruteForce:
+    @command.command("bruteforce")
+    def bruteforce(
+        self,
+        flows: Sequence[flow.Flow],
+        wordlist: str
+    ) -> None:
+        logging.debug("[bruteforce.py] log level is set to debug or higher.")
+        flowdex = 0
+        for f in flows:
+            if isinstance(f, http.HTTPFlow):
+                this_filename = f"request_{flowdex}.mitm"
+                this_outfile = open(this_filename, "wb")
+                this_outfile.write(raw_request(f))
+                this_outfile.close()
+                
+                this_ffuf_outfile = f"request_{flowdex}.ffuf"
+                arglist = ["ffuf","-request",this_filename,"-w",wordlist,"-o",this_ffuf_outfile]
+                subprocess.run(arglist)
+
+addons = [BruteForce()]
